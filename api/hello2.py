736341c312cc280cdd39a8c96f0d9c29e34d4926
Ashish Kankal <ashish25895@gmail.com>
2022-05-10 10:33:38 +0530

    added requirements

--- api/hello2.py
+++ api/hello2.py
@@ -1,5 +1,8 @@
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
+from urllib.request import urlopen
+import nbformat
+from nbconvert import PDFExporter
 
 
 class handler(BaseHTTPRequestHandler):
@@ -9,7 +12,16 @@ class handler(BaseHTTPRequestHandler):
         dic = dict(parse.parse_qsl(parse.urlsplit(s).query))
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
+
         self.end_headers()
+
+        url = 'https://jakevdp.github.io/downloads/notebooks/XKCD_plots.ipynb'
+        response = urlopen(url).read().decode()
+        jake_notebook = nbformat.reads(response, as_version=4)
+        print(jake_notebook)
+        pdf_exporter = PDFExporter()
+        pdf_exporter.from_notebook_node(jake_notebook)
+
         if "name" in dic:
             message = "Hello, " + dic["name"] + "!"
         else:
